be976584161bddcb4e4d4f78c61dba80f00e6575
NekroMagus <NekroMagus@mail.ru>
2020-05-26 11:12:57 +0300

    changed button, added https

--- src/components/landing/skideo/Skideo2.js
+++ src/components/landing/skideo/Skideo2.js
@@ -13,15 +13,18 @@ class Skideo2 extends Component {
   render() {
     return (
         <section className={css.slideSection}>
-            <div className={css.overlay}>
-              <div className="container">
-                <div className={css.content}>
-                  <h1 className="content--title">Skideo</h1>
-                  <p className="content--text">Upload your skills and get a chance</p>
-                  <button onClick={this.props.onOpenModal} className="btn open-modal">Добавить видео</button>
-                </div>
+          <div className={css.overlay}>
+            <div className="container">
+              <div className={css.content}>
+                <h1 className="content--title">Skideo</h1>
+                <p className="content--text">Upload your skills and get a chance</p>
+                <button
+                    onClick={this.props.isAuth ? this.props.redirect : this.props.onOpenModal}
+                    className="btn open-modal">Добавить видео
+                </button>
               </div>
             </div>
+          </div>
         </section>
     );
   }
